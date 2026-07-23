4831729150d41b99bb8b665efa305f777d125aed
vhag233392 <233392@ids.upchiapas.edu.mx>
2026-07-23 06:46:41 -0600

    Safe Areas

--- lib/features/clubs/presentation/views/club_captcha_view.dart
+++ lib/features/clubs/presentation/views/club_captcha_view.dart
@@ -74,7 +74,7 @@ class _ClubCaptchaViewState extends State<ClubCaptchaView> {
 
     return Scaffold(
       body: SafeArea(
-        child: Padding(
+        child: SingleChildScrollView(
           padding: const EdgeInsets.all(24),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
